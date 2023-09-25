74b8a67bf9c80278a922d743a95dac73712015e0
Ioan Plugar <84761756+plugarivan@users.noreply.github.com>
2023-09-25 15:51:57 +0300

    25-09

--- task14/14-23.py
+++ task14/14-23.py
@@ -1,23 +1,30 @@
 '''
-В системе счисления с основанием p выполняется равенство
+В системе счисления с некоторым основанием p выполняется равенство
 
-    1x77 + xx77 = y0yy
-Буквами x и y обозначены некоторые цифры из алфавита системы счисления с основанием p. Определите значение числа yxyxp и запишите это значение в десятичной системе счисления.
+    y3y + y65 = x2z0
+Буквами x, y и z обозначены некоторые цифры из алфавита системы счисления с основанием p. Запишите в ответе значение числа xyzp в десятичной системе счисления.
 
 '''
-import string
+'''for p in range(7, 100):
+    for x in range(p):
+        for y in range(p):
+            for z in range(p):
+                if (y * p ** 2 + 3 * p + y) + (y * p ** 2 + 6 * p + 5) == (x * p ** 3 + 2 * p ** 2 + z * p):
+                    print(x, y, z, p)
 
-A = string.digits + string.ascii_uppercase
+print(int('17a', 12))'''
 
-for p in range(2, 37):
-  for x in A[0:p]:
-    sx = f'1{x}77'
-    sy = f'{x}{x}77'
-    for y in A[1:p]:
-      ss = f'{y}0{y}{y}'
-      try:
-        if int( sx, p ) + int( sy, p) == int( ss, p ):
-          print( x, y, p )
-          print( int( f'{y}{x}{y}{x}', p ) )
-      except:
-        pass
+def f(a, n):
+  return sum(a[::-1][i] * n ** i for i in range(len(a)))
+
+
+for p in range(7, 100):
+    for x in range(p):
+        for y in range(p):
+            for z in range(p):
+                if f([y, 3, y], p) + f([y, 6, 5], p) == f([x, 2, z, 0], p):
+                    print(p, x, y, z, f([x, y, z], p))
+                    break
+
+
+
